58d24f9e8c351346632033ba9afc1ef931a4510f
Abhinav <abhinavsingh18813@gmail.com>
2024-07-13 01:48:47 +0530

    use modules

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -1,3 +1,5 @@
+import{cart} from '../data/cart.js';
+
 let productsHTML='';
 
 products.forEach((product) =>{
@@ -79,6 +81,15 @@ document.querySelectorAll('.js-add-to-cart')
           quantity: 1
         });
        }
+        
+       let cartQuantity=0;
+        cart.forEach((item) => {
+           cartQuantity += item.quantity;
+        });
+        
+        document.querySelector('.js-cart-quantity').innerHTML=cartQuantity;
+
+        console.log(cartQuantity);
         console.log(cart);
     });
 });
