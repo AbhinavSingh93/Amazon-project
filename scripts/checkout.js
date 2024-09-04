b2201138dff9ae83b85120b9b8228b6b4d4483a8
Abhinav <abhinavsingh18813@gmail.com>
2024-09-04 08:29:53 +0530

    covert from promises to async await

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -6,7 +6,24 @@ import { loadCart } from '../data/cart.js';
 //import '../data/cart-class.js';
 //import '../data/backend-practice.js';
  
- Promise.all([
+async function loadPage(){
+ 
+  await loadProductsFetch();
+  
+ const value= await new Promise((resolve)=>{
+    loadCart(()=>{
+      resolve('value3');
+    });
+  });
+  
+  renderOrderSummary();
+  renderPaymentSummary();
+ 
+}
+loadPage()
+
+
+ /*Promise.all([
   loadProductsFetch(),
    new Promise((resolve)=>{
     loadCart(()=>{
@@ -18,7 +35,7 @@ import { loadCart } from '../data/cart.js';
   console.log(values);
   renderOrderSummary();
   renderPaymentSummary();
- });
+ });*/
 
  /*new Promise((resolve)=>{
   loadProducts(()=>{
